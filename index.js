f01ead7807af105dff619ce11c3f2c7f3c8b31f5
Capricorn397 <chrisjcarr4@hotmail.co.uk>
2017-03-04 21:28:38 +0000

    Fixed bugs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const database = require('mysql')
 const restify = require('restify')
 const error = ''
 const port = 8000
@@ -8,7 +9,7 @@ const twoDP = 2
 const fsTopTen = 10
 const server = restify.createServer({
 	name: 'Dissertation_Server',
-	version: '0.0.1'
+	version: '0.0.5'
 })
 const httpCodes = {
 	OK: 200,
@@ -20,6 +21,15 @@ const httpCodes = {
 	requestTimeout: 408,
 	internalServerError: 500
 }
+
+const sql = database.createPool({
+	user: 'mainServer',
+	password: 'myDissertation',
+	host: '217.182.64.177',
+	port: 7999,
+	database: 'dissertation',
+	connectionLimit: 10
+})
 
 const questionStore = {}
 const answerStoreWord = {}
@@ -60,12 +70,12 @@ server.post('/questin', (req, res) => {
 	const modCode = req.headers.code
 	const question = req.body
 	let modTest = false
-	for i in moduleTokens {
-		if modCode === moduleTokens[i]{
+	for (let i = 0; i < moduleTokens.length; i++) {
+		if (modCode == moduleTokens[i]) {
 			modTest = true
 		}
 	}
-	if modTest === false {
+	if (modTest === false) {
 		res.send('Error No Registered Module')
 	}
 	else {
@@ -79,3 +89,11 @@ server.get('/input.html', restify.serveStatic({
 	directory: './views',
 	file: 'input.html'
 }))
+
+server.post('/register', (req, res) => {
+	const mod = req.headers.module
+	const unique = 'thing'//Get number from sql
+	const userCode = mod + unique
+	//Store user in database
+	res.send(userCode)
+})
